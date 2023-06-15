b86aaf5f6c29d8f64c22a6b91110574a1be8f91e
AndrewLin95 <andrew.lin88@hotmail.com>
2023-06-15 14:33:29 -0400

    updated project structure

--- client/src/app/project/map/page.tsx
+++ client/src/app/project/map/page.tsx
@@ -1,12 +1,17 @@
 "use client";
 import Header from "./Header";
 import { NextPage } from "next";
+import SearchModule from "../SearchModule";
+import MapModule from "./MapModule";
 
 const homepage: NextPage = () => {
   return (
     <div className="w-screen h-screen overflow-hidden flex flex-col justify-center items-center">
       <Header />
-      <div className="flex flex-row w-full h-full">test</div>
+      <div className="flex flex-row w-full h-full">
+        <SearchModule />
+        <MapModule />
+      </div>
     </div>
   );
 };
