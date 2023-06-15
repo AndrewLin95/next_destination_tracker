7a7e7925888624d58cd1e0cfbb1a5fb9745113aa
AndrewLin95 <andrew.lin88@hotmail.com>
2023-06-15 17:01:00 -0400

    added model for projectLocationData which will be used to store all search queries used to populate the map, schedule and notes

---
+++ server/src/models/projectSchema.ts
@@ -0,0 +1,66 @@
+import mongoose from "mongoose";
+
+const projectLocationDataSchema = new mongoose.Schema({
+  userID: {
+    type: String,
+    required: true,
+  },
+  projectID: {
+    type: String,
+    required: true,
+  },
+  mapData: {
+    formattedAddress: {
+      type: String,
+      required: true,
+    },
+    lat: {
+      type: Number,
+      required: true,
+    },
+    lng: {
+      type: Number,
+      required: true
+    },
+    googleLocationID: {
+      type: String,
+      required: true,
+    },
+  },
+  noteData: {
+    customName: {
+      type: String,
+      required: false,
+    },
+    customNote: {
+      type: String,
+      required: false,
+    },
+    openHours: {
+      type: String,
+      required: false,
+    },
+    closeHours: {
+      type: String,
+      required: false,
+    },
+  },
+  scheduleData: {
+    scheduleDate: {
+      type: Date,
+      required: false,
+    },
+    scheduleStart: {
+      type: Date,
+      required: false,
+    },
+    scheduleEnd: {
+      type: Date,
+      required: false,
+    }
+  }
+}, { versionKey: false })
+
+const ProjectLocationDataSchema = mongoose.model('ProjectLocationDataSchema', projectLocationDataSchema);
+
+module.exports = ProjectLocationDataSchema;
