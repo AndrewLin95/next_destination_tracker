38bf521e56fcb6f5e474d62130362595b1f70b0a
AndrewLin95 <andrew.lin88@hotmail.com>
2023-06-16 15:41:03 -0400

    updated styling for new project form

--- client/src/app/homepage/NewProject.tsx
+++ client/src/app/homepage/NewProject.tsx
@@ -2,41 +2,43 @@ import Image from "next/image";
 
 const NewProject = () => {
   return (
-    <div className="flex h-1/2 w-full border border-red-500 justify-center items-center">
-      <div className="flex flex-col border border-dashed w-4/12 h-5/6 p-6">
+    <div className="flex h-[420px] w-full justify-center items-center mt-8">
+      <div className="flex flex-col border border-dashed w-4/12 h-[420px] p-6">
         <form>
-          <div className="flex flex-col h-full">
-            <input
-              className="w-full mb-2"
-              placeholder="Target Destination"
-              name="destination"
-            />
-            <input
-              className="w-full mb-2"
-              placeholder="Project Name"
-              name="name"
-            />
-            <input
-              className="w-full mb-2"
-              placeholder="Project Description"
-              name="description"
-            />
-            <div className="flex flex-row mb-2">
-              <div className="w-64">Planned Start Date:</div>
-              <input className="w-full" type="date" name="startDate" />
-            </div>
-            <div className="flex flex-row mb-2">
-              <div className="w-64">Planned End Date:</div>
-              <input className="w-full" type="date" name="endDate" />
-            </div>
-            <Image
-              alt="vacation image"
-              src="/seasideVacation.jpg"
-              className="h-auto w-full max-h-40 object-cover"
-              width={288}
-              height={288}
-            />
-            <button type="submit">Track new project!</button>
+          <input
+            className="w-full mb-2"
+            placeholder="Target Destination"
+            name="destination"
+          />
+          <input
+            className="w-full mb-2"
+            placeholder="Project Name"
+            name="name"
+          />
+          <input
+            className="w-full mb-2"
+            placeholder="Project Description"
+            name="description"
+          />
+          <div className="flex flex-row mb-2">
+            <div className="w-64">Planned Start Date:</div>
+            <input className="w-full" type="date" name="startDate" />
+          </div>
+          <div className="flex flex-row mb-2">
+            <div className="w-64">Planned End Date:</div>
+            <input className="w-full" type="date" name="endDate" />
+          </div>
+          <Image
+            alt="vacation image"
+            src="/seasideVacation.jpg"
+            className="h-auto w-full max-h-40 object-cover"
+            width={288}
+            height={288}
+          />
+          <div className="flex justify-end w-full">
+            <button className="bg-PrimaryButton mt-3 w-72" type="submit">
+              Track new project!
+            </button>
           </div>
         </form>
       </div>
