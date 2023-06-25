eae54975bdb4394eea0adba3b8c2d8e1d438055e
AndrewLin95 <andrew.lin88@hotmail.com>
2023-06-25 12:03:39 -0400

    added expanded note info

---
+++ client/src/app/project/[projectid]/searchComponents/Note.tsx
@@ -0,0 +1,50 @@
+import { NoteData } from "@/util/models";
+import { FC, useState } from "react";
+import { SIMPLE_BUTTON_STYLE } from "@/util/constants";
+
+interface Props {
+  note: NoteData;
+}
+
+const Note: FC<Props> = ({ note }) => {
+  const [expandState, setExpandState] = useState(false);
+
+  // TODO: API calls to edit
+  // TODO: API call to embed and add picture
+  // TODO: Priority
+  return (
+    <div className="pb-8 w-full flex flex-col justify-center items-center">
+      <div className="flex justify-between w-full">
+        {note.noteName}
+        <button
+          type="button"
+          className={SIMPLE_BUTTON_STYLE}
+          onClick={() => setExpandState(!expandState)}
+        >
+          +
+        </button>
+      </div>
+      {expandState ? (
+        <div className="flex flex-col h-48 w-full border border-grey">
+          <div className="font-bold">
+            Address:{" "}
+            <div className="text-sm font-light">{note.formattedAddress}</div>
+          </div>
+          <div className="font-bold">
+            Notes:
+            <div className="text-sm font-light h-20">
+              {note.customNote ? note.customNote : "Enter a custom note"}
+            </div>
+          </div>
+          <div className="flex justify-end w-full font-light text-sm">
+            {note.openHours
+              ? `${note.openHours} - ${note.closeHours}`
+              : "Enter hours"}
+          </div>
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default Note;
--- client/src/app/project/[projectid]/searchComponents/SearchResults.tsx
+++ client/src/app/project/[projectid]/searchComponents/SearchResults.tsx
@@ -1,26 +1,25 @@
+import InLineTextButton from "@/components/InLineTextButton";
+import { NoteData } from "@/util/models";
 import { FC } from "react";
+import Note from "./Note";
+interface Props {
+  noteData: NoteData[];
+}
 
-const SearchResults: FC = ({}) => {
+const SearchResults: FC<Props> = ({ noteData }) => {
+  if (noteData.length === 0) {
+    return null;
+  }
+
   return (
     <div className="h-full">
       <div className="flex flex-row justify-center items-center p-4 text-2xl">
         <div className="pr-4 text-lg">Searched Places</div>
-        <button className="text-lg">Delete</button>
       </div>
       <div className="flex flex-col items-center h-full pb-32">
-        {/* <div v-for="(value, key) in searchHistoryDisplayData" 
-          className="pb-8 w-full flex justify-center items-center"
-        >
-          <input 
-            type="checkbox"
-            @click="handleDeleteFlagClickAction(($event.target as HTMLInputElement).checked, value.id)"
-            :checked="value.deleteFlag"
-            className="w-14 ml-12"
-          />
-          <div className="w-full">
-            {{ value.location }}
-          </div>
-        </div> */}
+        {noteData.map((note) => {
+          return <Note key={note.locationID} note={note} />;
+        })}
       </div>
     </div>
   );
