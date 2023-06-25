b182438a9446615640444f06e3441910582412f9
AndrewLin95 <andrew.lin88@hotmail.com>
2023-06-25 11:37:46 -0400

    added pagination sync with state

--- client/src/app/project/[projectid]/searchComponents/SearchPagination.tsx
+++ client/src/app/project/[projectid]/searchComponents/SearchPagination.tsx
@@ -1,10 +1,24 @@
 import { FC } from "react";
 
-const SearchPagination: FC = () => {
+interface Props {
+  paginationState: (string | number)[];
+}
+
+const SearchPagination: FC<Props> = ({ paginationState }) => {
+  if (paginationState.length === 0) {
+    return null;
+  }
+
   return (
     <div className="flex flex-row justify-center pb-4">
       <button className="w-12 h-12 border">{"<"}</button>
-      <button className="w-12 h-12 border"></button>
+      {paginationState.map((paginationValue, index) => {
+        return (
+          <button key={index} className="w-12 h-12 border">
+            {paginationValue}
+          </button>
+        );
+      })}
       <button className="w-12 h-12 border">{">"}</button>
     </div>
   );
