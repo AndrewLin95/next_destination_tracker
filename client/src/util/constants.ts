719010eadb6f5ec9114cf7bce9c9fb60ca09c3c8
AndrewLin95 <andrew.lin88@hotmail.com>
2023-06-25 12:03:25 -0400

    constant for button styling

--- client/src/util/constants.ts
+++ client/src/util/constants.ts
@@ -1,2 +1,3 @@
 export const maxNumOfImages: number = 1;
 export const NUM_RESULTS_PER_PAGE = 10;
+export const SIMPLE_BUTTON_STYLE = "font-bold text-blue-300 border-r-0 text-md p-0 bg-inherit hover:border-transparent focus:outline-none"
