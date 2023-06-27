6e538b928a9dfaa44e4008bf5754465cbe96eabf
AndrewLin95 <andrew.lin88@hotmail.com>
2023-06-27 15:28:16 -0400

    added more styling constants for dialog

--- client/src/util/constants.ts
+++ client/src/util/constants.ts
@@ -1,3 +1,10 @@
 export const maxNumOfImages: number = 1;
 export const NUM_RESULTS_PER_PAGE = 10;
-export const SIMPLE_BUTTON_STYLE = "font-bold text-blue-300 border-r-0 text-md p-0 bg-inherit hover:border-transparent focus:outline-none"
+
+export const SIMPLE_BUTTON_STYLE = "font-bold border-r-0 text-md p-0 bg-inherit hover:border-transparent focus:outline-none"
+export const FORM_SUBMIT_BUTTON = "font-bold border-r-0 text-md p-0 bg-PrimaryButton hover:border-transparent focus:outline-none"
+export const FORM_CANCEL_BUTTON = "font-bold border-r-0 text-md p-0 bg-SecondaryButton hover:border-transparent focus:outline-none"
+
+// Styling for editNoteDialog
+export const PRIORITY_SELECTED_STYLE = "pr-2 text-Accent";
+export const PRIORITY_DEFAULT_STYLE = "pr-2";
