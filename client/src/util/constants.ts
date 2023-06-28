4eae95a1ad9685f375598cbe661b57ecf07cf6da
AndrewLin95 <andrew.lin88@hotmail.com>
2023-06-28 15:32:49 -0400

    added constants for priority

--- client/src/util/constants.ts
+++ client/src/util/constants.ts
@@ -1,6 +1,10 @@
 export const maxNumOfImages: number = 1;
 export const NUM_RESULTS_PER_PAGE = 10;
 
+export const NOTE_PRIORITY_LOW: string = "Low";
+export const NOTE_PRIORITY_MED: string = "Medium";
+export const NOTE_PRIORITY_HIGH: string = "High";
+
 export const SIMPLE_BUTTON_STYLE = "font-bold border-r-0 text-md p-0 bg-inherit hover:border-transparent focus:outline-none"
 export const FORM_SUBMIT_BUTTON = "font-bold border-r-0 text-md p-0 bg-PrimaryButton hover:border-transparent focus:outline-none"
 export const FORM_CANCEL_BUTTON = "font-bold border-r-0 text-md p-0 bg-SecondaryButton hover:border-transparent focus:outline-none"
