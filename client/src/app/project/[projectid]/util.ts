6918e866b16645ba9b6f52c193095e82cb457245
AndrewLin95 <andrew.lin88@hotmail.com>
2023-06-30 16:24:16 -0400

    fixed bug with pagination when adding a new location entry

--- client/src/app/project/[projectid]/util.ts
+++ client/src/app/project/[projectid]/util.ts
@@ -4,9 +4,9 @@ import { Dispatch, SetStateAction } from 'react';
 export const handleValidatePagination = (type: string, locationData: LocationData[], numberOfPages: number, setNumberOfPages: Dispatch<SetStateAction<number>> ) => {
   let newNumberOfPages;
   if (type === "+") {
-    newNumberOfPages = Math.ceil((locationData.length + 1) / 10);
+    newNumberOfPages = Math.ceil(locationData.length / 10);
   } else {
-    newNumberOfPages = Math.ceil((locationData.length - 1) / 10);
+    newNumberOfPages = Math.ceil(locationData.length / 10);
   }
 
   if (newNumberOfPages !== numberOfPages) {
