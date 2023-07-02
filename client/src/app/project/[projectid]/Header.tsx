9fadfa09f6ff60ae887b7da38654596fd637d5f1
AndrewLin95 <andrew.lin88@hotmail.com>
2023-07-02 14:32:05 -0400

    formatting HeaderTitle

--- client/src/app/project/[projectid]/Header.tsx
+++ client/src/app/project/[projectid]/Header.tsx
@@ -4,7 +4,6 @@ const Header = () => {
   return (
     <div className="w-screen h-16 flex flex-row justify-between items-center">
       <HeaderTitle />
-      <button>Get Current Location</button>
       <div className="pl-8">
         <div className="font-bold">Current Location</div>
       </div>
--- client/src/components/headerComponents/HeaderTitle.tsx
+++ client/src/components/headerComponents/HeaderTitle.tsx
@@ -4,21 +4,23 @@ import { useRouter } from "next/navigation";
 const HeaderTitle = () => {
   const router = useRouter();
   return (
-    <button
-      type="button"
-      className="ml-8 pt-2"
-      onClick={() => {
-        router.push("/homepage");
-      }}
-    >
-      <Image
-        src="/DestinationLogoTransparent.png"
-        alt="Destination"
-        width={144}
-        height={144}
-        className="mix-blend-plus-lighter"
-      />
-    </button>
+    <div className="pl-8">
+      <button
+        type="button"
+        className="pt-2"
+        onClick={() => {
+          router.push("/homepage");
+        }}
+      >
+        <Image
+          src="/DestinationLogoTransparent.png"
+          alt="Destination"
+          width={144}
+          height={144}
+          className="mix-blend-plus-lighter"
+        />
+      </button>
+    </div>
   );
 };
 
