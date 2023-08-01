9410cb575b64630d4edde1e753ba94a4675c8863
AndrewLin95 <andrew.lin88@hotmail.com>
2023-08-01 15:35:40 -0400

    added color picker to the form element

--- client/src/app/project/[projectid]/components/DateColorPicker.tsx
+++ client/src/app/project/[projectid]/components/DateColorPicker.tsx
@@ -1,6 +1,6 @@
 import { FC, useState } from "react";
 import { DAYS_OF_WEEK, SIMPLE_BUTTON_STYLE } from "@/util/constants";
-import { ChromePicker, ChromePickerProps } from "react-color";
+import { CompactPicker } from "react-color";
 
 interface Props {
   dayOfWeek: DAYS_OF_WEEK;
@@ -9,23 +9,42 @@ interface Props {
 
 const DateColorPicker: FC<Props> = ({ dayOfWeek, color }) => {
   const [togglePicker, setTogglePicker] = useState(false);
-  const [newcolor, setNewColor] = useState(undefined);
+  const [newColor, setNewColor] = useState(color);
 
   const handleChange = (e: any) => {
-    console.log(e);
+    setNewColor(e.hex);
   };
 
   return (
-    <div className="flex flex-row items-center">
-      <div className="pr-2">{dayOfWeek}</div>
-      <button
-        type="button"
-        className={`${SIMPLE_BUTTON_STYLE} h-8 w-8`}
-        style={{ backgroundColor: color }}
-        onClick={() => setTogglePicker(true)}
-      ></button>
+    <div className="flex flex-col justify-center pr-1">
+      <div className="flex flex-row">
+        <button
+          type="button"
+          className={`${SIMPLE_BUTTON_STYLE} h-8 w-8`}
+          style={{ backgroundColor: newColor }}
+          onClick={() => setTogglePicker(true)}
+        />
+        <label className="w-24 pl-2" style={{ color: newColor }}>
+          {dayOfWeek}
+        </label>
+        <input value={newColor} type="hidden" name={`${dayOfWeek}Color`} />
+      </div>
       {togglePicker ? (
-        <ChromePicker color={color} onChangeComplete={(e) => handleChange(e)} />
+        <>
+          <div
+            className="absolute h-full w-full top-1 left-1"
+            onClick={() => setTogglePicker(false)}
+          />
+          <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
+            <div style={{ color: newColor }} className="bg-slate-400/90">
+              {dayOfWeek}
+            </div>
+            <CompactPicker
+              color={newColor}
+              onChangeComplete={(e) => handleChange(e)}
+            />
+          </div>
+        </>
       ) : null}
     </div>
   );
--- client/src/app/project/[projectid]/components/ScheduleSettingsDialog.tsx
+++ client/src/app/project/[projectid]/components/ScheduleSettingsDialog.tsx
@@ -11,21 +11,60 @@ const ScheduleSettingsDialog: FC<Props> = ({
   setScheduleSettingsToggle,
   projectData,
 }) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(e);
+    console.log((e.target as HTMLFormElement).SundayColor.value);
+  };
+
   return (
     <>
       <div
         className="absolute h-screen w-screen bg-slate-900/40"
         onClick={() => setScheduleSettingsToggle(false)}
       />
-      <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[32rem] h-[18rem] bg-Background flex flex-col p-4">
+      <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[35rem] h-[18rem] bg-Background flex flex-col p-4">
         <div className="text-xl font-bold pb-2 underline text-Accent">
           Edit Schedule Settings
         </div>
-        <form>
-          <DateColorPicker
-            dayOfWeek={"Monday"}
-            color={projectData.scheduleColors.Monday}
-          />
+        <form onSubmit={handleSubmit}>
+          <div className="underline pb-2 pl-1">Schedule Colors</div>
+          <div className="flex flex-row pb-3">
+            <DateColorPicker
+              dayOfWeek={"Monday"}
+              color={projectData.scheduleColors.Monday}
+            />
+            <DateColorPicker
+              dayOfWeek={"Tuesday"}
+              color={projectData.scheduleColors.Tuesday}
+            />
+            <DateColorPicker
+              dayOfWeek={"Wednesday"}
+              color={projectData.scheduleColors.Wednesday}
+            />
+            <DateColorPicker
+              dayOfWeek={"Thursday"}
+              color={projectData.scheduleColors.Thursday}
+            />
+          </div>
+          <div className="flex flex-row pb-3">
+            <DateColorPicker
+              dayOfWeek={"Friday"}
+              color={projectData.scheduleColors.Friday}
+            />
+            <DateColorPicker
+              dayOfWeek={"Saturday"}
+              color={projectData.scheduleColors.Saturday}
+            />
+            <DateColorPicker
+              dayOfWeek={"Sunday"}
+              color={projectData.scheduleColors.Sunday}
+            />
+          </div>
+          <div className="underline pb-2 pl-1">
+            Schedule Start and End Times
+          </div>
+          <button type="submit">Submit Test</button>
         </form>
       </div>
     </>
