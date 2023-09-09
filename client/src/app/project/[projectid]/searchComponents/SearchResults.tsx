a0ea0d7eb3a82146c12ae2df95a4465d8208ce0a
skim2264 <72099715+skim2264@users.noreply.github.com>
2023-09-08 21:10:43 -0400

    Sorting feature by name, date, or priority added

--- client/src/app/project/[projectid]/searchComponents/SearchResults.tsx
+++ client/src/app/project/[projectid]/searchComponents/SearchResults.tsx
@@ -1,5 +1,5 @@
 import { NoteData, ScheduleColors } from "@/util/models/ProjectModels";
-import { FC } from "react";
+import { FC, useState } from "react";
 import Note from "./Note";
 import { VIEW_TYPES } from "@/util/constants";
 
@@ -24,6 +24,29 @@ const SearchResults: FC<Props> = ({
   handleDrag,
   scheduleColors,
 }) => {
+  const [expandSort, setExpandSort] = useState(false);
+  const [data, setData] = useState(noteData);
+
+  const sortByName = () => {
+    data.sort((a,b) => a.noteName.localeCompare(b.noteName));
+    setData(data);
+    setExpandSort(false);
+  };
+
+  const sortByDate = () => {
+    data.sort((a,b) => a.scheduleDate ? b.scheduleDate ? a.scheduleDate - b.scheduleDate : -1 : 1);
+    setData(data);
+    setExpandSort(false);
+  };
+
+  const sortByPriority = () => {
+    const order = { 'Low': 1, 'Medium': 2, 'High': 3 }
+    //Not sure why this error occurs
+    data.sort((a,b) => order[b.priority] - order[a.priority]);
+    setData(data);
+    setExpandSort(false);
+  };
+
   if (noteData.length === 0) {
     return null;
   }
@@ -33,8 +56,20 @@ const SearchResults: FC<Props> = ({
       <div className="flex flex-row justify-center items-center pb-4 text-2xl">
         <div className="pr-4 text-lg font-bold">Searched Places</div>
       </div>
-      <div className="flex flex-col items-center h-[calc(100vh-15rem)] overflow-y-auto">
-        {noteData.map((note) => {
+
+      <button onClick={() => setExpandSort(!expandSort)}>Sort</button>
+      {expandSort ? (
+        <div>
+          <ul>
+            <li><button onClick={sortByName}>by Name</button></li>
+            <li><button onClick={sortByDate}>by Date</button></li>
+            <li><button onClick={sortByPriority}>by Priority</button></li>
+          </ul>
+        </div>
+      ) : null}
+
+      <div className="flex flex-col items-center h-[calc(100vh-15rem)] overflow-y-auto pt-4">
+        {data.map((note) => {
           return (
             <Note
               key={note.locationID}
@@ -47,7 +82,7 @@ const SearchResults: FC<Props> = ({
               handleDrag={handleDrag}
               scheduleColors={scheduleColors}
             />
-          );
+          )
         })}
       </div>
     </div>
