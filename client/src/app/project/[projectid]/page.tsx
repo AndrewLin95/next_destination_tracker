6f6c58568cc218b0f631922d4193ec45e896986e
skim2264 <72099715+skim2264@users.noreply.github.com>
2023-09-18 17:36:32 -0400

    Sort and filter note data from parent component

--- client/src/app/project/[projectid]/page.tsx
+++ client/src/app/project/[projectid]/page.tsx
@@ -106,6 +106,42 @@ const ProjectPage: NextPage<Props> = ({ params }) => {
   const [scheduleSettingsToggle, setScheduleSettingsToggle] =
     useState<Boolean>(false);
 
+  //Filter/sort Data
+  const [expandModal, setExpandModal] = useState(false);
+  const [locationIDArray, setLocationIDArray] = useState<string []>([]);
+  const [filteredNoteData, setFilteredNoteData] = useState<NoteData[]>([]);
+  const [filterValues, setFilterValues] = useState<string []>([]);
+  const [lastFilterValues, setLastFilterValues] = useState<string []>([]);
+  const [sortValue, setSortValue] = useState("");
+
+  const handleFilterChange = (newVal: string) => {
+    if (filterValues.includes(newVal)) {
+      setFilterValues((curr) => curr.filter((val) => val != newVal));
+    } else {
+      setFilterValues([...filterValues, newVal]);
+    }
+  };
+
+  const applyFilter = () => {
+    const tempIDs: string[] = [];
+
+    if (filterValues.length != 0) {
+      const filteredData = noteData.filter((note) => filterValues.includes(note.priority));
+      filteredData.forEach((note)=>{tempIDs.push(note.locationID)});
+      setLocationIDArray(tempIDs);
+      setFilteredNoteData(filteredData);
+    } else {
+      setLocationIDArray([]);
+    }
+    setLastFilterValues([...filterValues]);
+    setExpandModal(false);
+  };
+
+  const cancelFilter = () => {
+    setExpandModal(false);
+    setFilterValues([...lastFilterValues]);
+  };
+
   useEffect(() => {
     if (
       params.projectid === undefined ||
@@ -152,6 +188,7 @@ const ProjectPage: NextPage<Props> = ({ params }) => {
       });
       setMapData(tempMapData);
       setNoteData(tempNoteData);
+      setFilteredNoteData(tempNoteData);
 
       const initMapCenter = {
         lat: parseFloat(responseData.projectData.project.projectCoords.lat),
@@ -591,8 +628,42 @@ const ProjectPage: NextPage<Props> = ({ params }) => {
                 setSearchText={setSearchText}
                 handleSearch={handleSearch}
               />
+              <button onClick={() => {
+                setExpandModal(!expandModal);
+              }}>Sort/Filter</button>
+
+              {expandModal ? (
+                <div className="h-full flex justify-center items-center bg-gray-600/50 absolute inset-0">
+                  <div className="flex justify-center flex-col items-center rounded-lg bg-white py-5 px-20 relative z-50">
+                    <p className="font-medium text-2xl mb-4">Sort</p>
+                    <ul>
+                      <li><button onClick={() => setSortValue("name")}>by Name</button></li>
+                      <li><button onClick={() => setSortValue("date")}>by Date</button></li>
+                      <li><button onClick={() => setSortValue("priority")}>by Priority</button></li>
+                    </ul>
+
+                    <p className="font-medium text-2xl my-4">Filter</p>
+                    <ul className="flex flex-col space-y-2 mb-5">
+                      <li>
+                        <input type="checkbox" id="priorityLow" name="Low" value="Low" checked={filterValues.includes("Low")} onChange={() => handleFilterChange("Low")}/>
+                        <label htmlFor="priorityLow"> Low Priority</label>
+                      </li>
+                      <li>
+                        <input type="checkbox" id="priorityMedium" name="Medium" value="Medium" checked={filterValues.includes("Medium")} onChange={() => handleFilterChange("Medium")}/>
+                        <label htmlFor="priorityMedium"> Medium Priority</label>
+                      </li>
+                      <li>
+                        <input type="checkbox" id="priorityHigh" name="High" value="High" checked={filterValues.includes("High")} onChange={() => handleFilterChange("High")}/>
+                        <label htmlFor="priorityHigh"> High Priority</label>
+                      </li>
+                    </ul>
+                    <button onClick={cancelFilter}>Cancel</button>
+                    <button onClick={applyFilter}>Apply Filters</button>
+                  </div>
+                </div>
+              ) : null}
               <SearchResults
-                noteData={noteData}
+                noteData={filteredNoteData}
                 handleEditNoteDialog={handleEditNoteDialog}
                 handleDeleteNote={handleDeleteNote}
                 activeLocationID={activeLocationID}
@@ -600,6 +671,7 @@ const ProjectPage: NextPage<Props> = ({ params }) => {
                 viewToggle={viewToggle}
                 handleDrag={handleDrag}
                 scheduleColors={projectData.scheduleColors}
+                sortValue={sortValue}
               />
               <SearchPagination
                 paginationState={paginationState}
--- client/src/app/project/[projectid]/searchComponents/SearchResults.tsx
+++ client/src/app/project/[projectid]/searchComponents/SearchResults.tsx
@@ -12,6 +12,7 @@ interface Props {
   viewToggle: VIEW_TYPES;
   handleDrag: (e: React.DragEvent<HTMLDivElement>, note: NoteData) => void;
   scheduleColors: ScheduleColors;
+  sortValue:string
 }
 
 const SearchResults: FC<Props> = ({
@@ -23,113 +24,45 @@ const SearchResults: FC<Props> = ({
   viewToggle,
   handleDrag,
   scheduleColors,
+  sortValue
 }) => {
-  const [expandModal, setExpandModal] = useState(false);
-  const [data, setData] = useState(noteData);
-  const [filterValues, setFilterValues] = useState<string []>([]);
-  const [lastFilterValues, setLastFilterValues] = useState<string []>([]);
-  const [sortValue, setSortValue] = useState("");
-  const [resort, setResort] = useState(false);
 
-  const sortByValue = (value: string) => {
-    if (value === "name") {
-      data.sort((a,b) => a.noteName.localeCompare(b.noteName));
-    }
-    else if (value === "date") {
-      data.sort((a,b) => a.scheduleDate ? b.scheduleDate ? a.scheduleDate - b.scheduleDate : -1 : 1);
-    }
-    else if (value === "priority") {
-      const order = { 'Low': 1, 'Medium': 2, 'High': 3 }
-      data.sort((a,b) => order[b.priority] - order[a.priority]);
-    } else {
-      setData(noteData);
-    }
-    setSortValue(value);
-    setData([...data]);
-  };
-
-  const handleFilterChange = (newVal: string) => {
-    if (filterValues.includes(newVal)) {
-      setFilterValues((curr) => curr.filter((val) => val != newVal));
-    } else {
-      setFilterValues([...filterValues, newVal]);
-    }
-  };
-
-  const applyFilter = () => {
-    if (filterValues.length != 0) {
-      const filteredData = noteData.filter((note) => filterValues.includes(note.priority));
-      setData(filteredData);
-    } else {
-      setData(noteData);
-    }
-    setResort(true);
-    setLastFilterValues([...filterValues]);
-    setExpandModal(false);
-  };
-
-  const cancelFilter = () => {
-    setExpandModal(false);
-    setFilterValues([...lastFilterValues]);
-  };
+  const [noteDisplayData, setNoteDisplayData] = useState<NoteData[]>([...noteData]);
 
   useEffect(() => {
-    if (resort) {
-      if (sortValue != "") {
-        sortByValue(sortValue);
+    const sortByValue = (value: string) => {
+      var tempNoteData: NoteData[] = [...noteData];
+  
+      if (value === "name") {
+        tempNoteData.sort((a,b) => a.noteName.localeCompare(b.noteName));
       }
-    }
-    setResort(false);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[resort]);
+      else if (value === "date") {
+        tempNoteData.sort((a,b) => a.scheduleDate ? b.scheduleDate ? a.scheduleDate - b.scheduleDate : -1 : 1);
+      }
+      else if (value === "priority") {
+        const order = { 'Low': 1, 'Medium': 2, 'High': 3 }
+        tempNoteData.sort((a,b) => order[b.priority] - order[a.priority]);
+      } 
+  
+      if (value != "") setNoteDisplayData(tempNoteData);
+    };
+
+    sortByValue(sortValue);
+
+  },[sortValue, noteData]);
 
   if (noteData.length === 0) {
     return null;
-  }
-
+  } 
+  
   return (
     <div className="h-full">
       <div className="flex flex-row justify-center items-center pb-4 text-2xl">
         <div className="pr-4 text-lg font-bold">Searched Places</div>
       </div>
 
-      <button onClick={() => {
-        setExpandModal(!expandModal);
-      }}>Sort/Filter</button>
-
-      {expandModal ? (
-        <div className="h-full flex justify-center items-center bg-gray-600/50 absolute inset-0">
-          <div className="flex justify-center flex-col items-center rounded-lg bg-white py-5 px-20 relative z-50">
-            <p className="font-medium text-2xl mb-4">Sort</p>
-            <ul>
-              <li><button onClick={() => sortByValue("name")}>by Name</button></li>
-              <li><button onClick={() => sortByValue("date")}>by Date</button></li>
-              <li><button onClick={() => sortByValue("priority")}>by Priority</button></li>
-            </ul>
-
-            <p className="font-medium text-2xl my-4">Filter</p>
-            <ul className="flex flex-col space-y-2 mb-5">
-              <li>
-                <input type="checkbox" id="priorityLow" name="Low" value="Low" checked={filterValues.includes("Low")} onChange={() => handleFilterChange("Low")}/>
-                <label htmlFor="priorityLow"> Low Priority</label>
-              </li>
-              <li>
-                <input type="checkbox" id="priorityMedium" name="Medium" value="Medium" checked={filterValues.includes("Medium")} onChange={() => handleFilterChange("Medium")}/>
-                <label htmlFor="priorityMedium"> Medium Priority</label>
-              </li>
-              <li>
-                <input type="checkbox" id="priorityHigh" name="High" value="High" checked={filterValues.includes("High")} onChange={() => handleFilterChange("High")}/>
-                <label htmlFor="priorityHigh"> High Priority</label>
-              </li>
-            </ul>
-            <button onClick={cancelFilter}>Cancel</button>
-            <button onClick={applyFilter}>Apply Filters</button>
-          </div>
-        </div>
-      ) : null}
-
       <div className="flex flex-col items-center h-[calc(100vh-15rem)] overflow-y-auto pt-4">
-        {data.map((note) => {
+        {noteDisplayData.map((note) => {
           return (
             <Note
               key={note.locationID}
