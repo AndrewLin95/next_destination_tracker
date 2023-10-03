1e339fc21ae370fb7bd094409481a37fec729aa6
AndrewLin95 <andrew.lin88@hotmail.com>
2023-10-03 19:24:23 -0400

    typo

--- server/index.ts
+++ server/index.ts
@@ -22,7 +22,7 @@ app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow_Origin', '*');
+  res.header('Access-Control-Allow-Origin', '*');
   next();
 })
 
